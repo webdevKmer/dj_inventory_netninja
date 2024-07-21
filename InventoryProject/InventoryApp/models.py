a39900a1f71a93f7f5c4f6062deae780f29afed9
webdevKmer <webdevKmer@gmail.com>
2024-07-21 05:34:29 +0000

    create product model and make migrations

--- InventoryProject/InventoryApp/models.py
+++ InventoryProject/InventoryApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    sku = models.CharField(max_length=40)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    supplier = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
