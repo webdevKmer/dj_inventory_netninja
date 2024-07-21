02f45b9bbfbf4819021e70183ec52f6b54405b9a
webdevKmer <webdevKmer@gmail.com>
2024-07-21 05:35:03 +0000

    fix app urls and add views

--- InventoryProject/InventoryApp/views.py
+++ InventoryProject/InventoryApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'InventoryApp/home.html')
+
+def index(request):
+    return render(request, 'InventoryApp/index.html')
